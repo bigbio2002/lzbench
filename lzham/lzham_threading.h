9d154d8a13b4a923ca2c740e4ba121882b4aacbe
Harry Weinstein <449154+bigbio2002@users.noreply.github.com>
2025-04-02 00:55:34 -0400

    lzham: update license; redefine INT_MIN/MAX with LZHAM_* prefix

--- lzham/lzham_threading.h
+++ lzham/lzham_threading.h
@@ -1,13 +1,12 @@
 // File: lzham_threading.h
-// See Copyright Notice and license at the end of include/lzham.h
+// LZHAM is in the Public Domain. Please see the Public Domain declaration at the end of include/lzham.h
 
-/*#if LZHAM_USE_WIN32_API
+#if LZHAM_USE_WIN32_API
    #include "lzham_win32_threading.h"
 #elif LZHAM_USE_PTHREADS_API
    #include "lzham_pthreads_threading.h"
 #else
    #include "lzham_null_threading.h"
 #endif
-*/
-#include "lzham_null_threading.h"
+
 
